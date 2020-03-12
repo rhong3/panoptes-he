e8a9339f4e7af3481ebfefa03b0a4fd75e506805
rh2740 <rh2740@nyu.edu>
2020-03-12 12:26:11 -0400

    3.12.1

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as readme_file:
     readme = readme_file.read()
 
-requirements = ["easygui>=0.98", "Keras>=2.3", "numpy>=1.17.5", "opencv-python>=4.1.2",
+requirements = ["easygui>=0.98", "Keras>=2.2.4", "numpy>=1.17.5", "opencv-python>=4.1.2",
                 "openslide-python>=1.1.1", "pandas>=0.25", "Pillow>=7", "scikit-learn>=0.22", "scipy>=1.3",
                 "staintools>=2.1.2", "tensorflow>=1.11", "matplotlib>=3.1"]
 
 setup(
     name="panoptes-he",
-    version="0.1.12",
+    version="0.1.13",
     author="Runyu Hong",
     author_email="Runyu.Hong@nyu.edu",
     description="A multi-resolution CNN to predict endometrial cancer features",
